6a33156f500b0951e6eab56f78a7f01b2d6d2eee
Chris Franco <11533365+Francommit@users.noreply.github.com>
2019-04-13 02:07:57 +1000

    Added evolution

--- setup.sh
+++ setup.sh
@@ -2,3 +2,9 @@
 sudo apt-add-repository -y ppa:teejee2008/ppa
 sudo apt-get update
 sudo apt-get install timeshift -y
+
+# Evolution
+sudo apt-get remove evolution -y
+sudo apt-get update
+sudo apt-get install evolution -y
+sudo apt-get install evolution-ews -y
