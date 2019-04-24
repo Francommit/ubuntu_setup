63f0bbb25e2cba06da23e6c3828912a7a1f77e52
Chris Franco <11533365+Francommit@users.noreply.github.com>
2019-04-24 21:40:48 +1000

    Added 19.04 installation file

---
+++ setup-19.04.sh
@@ -0,0 +1,24 @@
+# Evolution
+sudo apt-get install evolution -y
+sudo apt-get install evolution-ews -y
+
+# Sublime Text 3
+sudo snap install sublime-text --classic
+
+# Chrome
+sudo snap install chromium
+
+# Powershell
+sudo snap install powershell --classic
+
+# Postman
+sudo snap install postman
+
+# Remmina
+sudo snap install remmina
+
+# Paint
+sudo snap install kolourpaint
+
+# VLC
+sudo snap install vlc
