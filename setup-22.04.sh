f2f0c6319322d5a83197ac3e1adfdeff1a982ca3
Chris Franco <11533365+Francommit@users.noreply.github.com>
2023-05-20 17:00:21 +1000

    Update setup-22.04.sh

--- setup-22.04.sh
+++ setup-22.04.sh
@@ -2,3 +2,5 @@ sudo snap install sublime-text --classic
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install gnome-shell-extension-autohidetopbar
+sudo snap install python-ai-toolkit
+
