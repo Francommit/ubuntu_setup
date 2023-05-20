4c9a52ce15a698f399cb06620bc28382f7c2ddc6
Chris Franco <11533365+Francommit@users.noreply.github.com>
2023-05-20 18:57:41 +1000

    Update setup-22.04.sh

--- setup-22.04.sh
+++ setup-22.04.sh
@@ -3,3 +3,8 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install gnome-shell-extension-autohidetopbar
 sudo apt install python3-pip -y
+sudo apt install -y aria2 -y 
+
+pip install jupyterlab
+pip install jupyter_http_over_ws
+jupyter serverextension enable --py jupyter_http_over_ws
