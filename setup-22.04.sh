b1102df2753d7791405096c9cb525131b186b2e8
Chris Franco <11533365+Francommit@users.noreply.github.com>
2023-05-20 18:56:24 +1000

    Update setup-22.04.sh

--- setup-22.04.sh
+++ setup-22.04.sh
@@ -3,4 +3,3 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install gnome-shell-extension-autohidetopbar
 sudo apt install python3-pip -y
-sudo snap install juypter
